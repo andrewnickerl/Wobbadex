fe8d4f093bb0c5ee83d733ee2cfaa0540f6aaa8d
Andrew Nickerl <andrew.nickerl@gmail.com>
2020-03-07 13:10:21 -0500

    Create Enumerations class and add an enum for Categories and an enum for TypeNames

---
+++ code/WobbadexClassLibrary/Enumerations.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WobbadexClassLibrary
+{
+    public class Enumerations
+    {
+        public enum TypeNames
+        {
+            Bug,
+            Dark,
+            Dragon,
+            Electric,
+            Fairy,
+            Fighting,
+            Fire,
+            Flying,
+            Ghost,
+            Grass,
+            Ground,
+            Ice,
+            Normal,
+            Poison,
+            Psychic,
+            Rock,
+            Steel,
+            Water
+        }
+
+        public enum Categories
+        {
+            Physical,
+            Special,
+            Status
+        }
+    }
+}
