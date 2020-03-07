59e26b1f11b4b0d27c7af9931c32b362f81b6dd4
Andrew Nickerl <andrew.nickerl@gmail.com>
2020-03-07 13:10:04 -0500

    Create Move class and add properties

---
+++ code/WobbadexClassLibrary/Move.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static WobbadexClassLibrary.Enumerations;
+
+namespace WobbadexClassLibrary
+{
+    public class Move
+    {
+        public string Name { get; set; }
+        public int Power { get; set; }
+        public Types Type { get; set; }
+        public Categories Category { get; set; }
+        public int PowerPoints { get; set; }
+        public int Accuracy { get; set; }
+        public string LearningMethod { get; set; }
+    }
+}
