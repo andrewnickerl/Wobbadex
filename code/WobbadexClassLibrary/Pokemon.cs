9da6e7d39e0517c012b2a687243e8c1ec2cf2cd6
Andrew Nickerl <andrew.nickerl@gmail.com>
2020-03-07 13:09:52 -0500

    Create Pokemon class and add properties

---
+++ code/WobbadexClassLibrary/Pokemon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using static WobbadexClassLibrary.Enumerations;
+
+namespace WobbadexClassLibrary
+{
+    public class Pokemon
+    {
+        public string Name { get; set; }
+        public Types Types { get; set; }
+        public List<string> Abilities { get; set; }
+        public List<Pokemon> Evolutions { get; set; }
+        public int Hp { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int SpAtk { get; set; }
+        public int SpDef { get; set; }
+        public int Speed { get; set; }
+        public List<Move> MoveList { get; set; }
+        public Bitmap Sprite { get; set; }
+    }
+}
