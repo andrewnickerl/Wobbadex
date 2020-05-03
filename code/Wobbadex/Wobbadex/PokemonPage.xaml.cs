3daa01b9440e8bfed1d9d5f6807a5a4ede1ba1f6
Andrew Nickerl <andrew.nickerl@gmail.com>
2020-05-03 14:26:22 -0400

    Instead of pulling pokemon information from the database and building a DataGrid in the class constructor, I built functions in conjunction with the items added to the xaml page to facilitate Pokemon information being pulled based on the search parameters

--- code/Wobbadex/Wobbadex/PokemonPage.xaml.cs
+++ code/Wobbadex/Wobbadex/PokemonPage.xaml.cs
@@ -27,115 +27,99 @@ namespace Wobbadex
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class PokemonPage : Page
-    {
+    {       
+        //Connection string using Window Authentication
+        private readonly string connectionString =
+        @"Server = DESKTOP-QA4COAS\SQLEXPRESS; Database = Wobbadex; Trusted_Connection = True";
+        //determines what data is pulled from the database table
+        private string query = "SELECT * FROM pokemon";
+        private string whereClause;
+        private string searchParameter;
+
+        //page constructor
         public PokemonPage()
         {
-            this.InitializeComponent();
-            //this.pokemonRepository = GetPokemon(this.connectionString);
+            this.InitializeComponent();            
+        }       
 
+        private void SearchParameter_Click(object sender, RoutedEventArgs e)
+        {
+            string option = ((MenuFlyoutItem)sender).Tag.ToString();
+            this.searchParameter = option;
+            
+            switch (option)
+            {
+                case "pokemon_name":
+                    searchDropDown.Content = "Name";
+                    break;
+                case "pokedex_number":
+                    searchDropDown.Content = "Pokedex Number";
+                    break;
+                case "type":
+                    searchDropDown.Content = "Type";
+                    break;
+                case "is_legendary":
+                    searchDropDown.Content = "Legendary";                    
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void RunSearch_Click(object sender, RoutedEventArgs e)
+        {
+            switch (this.searchParameter)
+            {
+                case "pokemon_name":
+                    this.whereClause = $"WHERE {this.searchParameter} like \'{searchTextBox.Text}\'";
+                    break;
+                case "pokedex_number":
+                    this.whereClause = $"WHERE {this.searchParameter} like \'{searchTextBox.Text}\'";
+                    break;
+                case "type":
+                    this.whereClause = $"WHERE type1 like \'{searchTextBox.Text}\' or type2 like \'{searchTextBox.Text}\'";
+                    break;
+                case "is_legendary":
+                    this.whereClause = $"WHERE {this.searchParameter} like \'{searchTextBox.Text}\'";
+                    break;
+                default:
+                    break;
+            }
+
+            DataTable pokemonTable = GetPokemon(this.connectionString, $"{this.query} {this.whereClause}");
+
+            for (int i = 0; i < pokemonTable.Columns.Count; i++)
+            {
+                PokemonGrid.Columns.Add(new DataGridTextColumn()
+                {
+                    Header = pokemonTable.Columns[i].ColumnName,
+                    Binding = new Binding { Path = new PropertyPath("[" + i.ToString() + "]") }
+                });
+            }
+            var pokemonCollection = new ObservableCollection<object>();
+            foreach (DataRow row in pokemonTable.Rows)
+            {
+                pokemonCollection.Add(row.ItemArray);
+            }
+
+            PokemonGrid.ItemsSource = pokemonCollection;
+        }
+
+        public DataTable GetPokemon(string connectionString, string query)
+        {
             using (var connection = new SqlConnection(connectionString))
             {
                 connection.Open();
                 var command = connection.CreateCommand();
-                command.CommandText = "SELECT * FROM pokemon";
+                command.CommandText = query;
                 DataTable pokemonTable = new DataTable();
 
                 using (var dataAdapter = new SqlDataAdapter(command))
                 {
                     dataAdapter.Fill(pokemonTable);
                 }
-
-                for (int i = 0; i < pokemonTable.Columns.Count; i++)
-                {
-                    PokemonGrid.Columns.Add(new DataGridTextColumn() 
-                    {
-                        Header = pokemonTable.Columns[i].ColumnName,
-                        Binding = new Binding { Path = new PropertyPath("[" + i.ToString() + "]") }
-                    });
-                }
-
-                var pokemonCollection = new ObservableCollection<object>();
-                foreach(DataRow row in pokemonTable.Rows)
-                {
-                    pokemonCollection.Add(row.ItemArray);
-                }
-
-                PokemonGrid.ItemsSource = pokemonCollection;
+                return pokemonTable;
             }
-            
-        }
-
-        //public readonly ObservableCollection<Pokemon> pokemonRepository;
-        ////Connection string using Window Authentication
-        private readonly string connectionString =
-        @"Server = DESKTOP-QA4COAS\SQLEXPRESS; Database = Wobbadex; Trusted_Connection = True";
-
-        ////populates pokemonRepository from DB using connectionString
-        //public ObservableCollection<Pokemon> GetPokemon(string connectionString)
-        //{
-        //    const string GetPokemonQuery = "SELECT * FROM pokemon";
-
-        //    var pokemonCollection = new ObservableCollection<Pokemon>();
-
-        //    SqlConnection conn = new SqlConnection(connectionString);
-        //    conn.Open();
-        //    if (conn.State == System.Data.ConnectionState.Open)
-        //    {
-        //        SqlCommand cmd = conn.CreateCommand();
-        //        cmd.CommandText = GetPokemonQuery;
-        //        SqlDataReader reader = cmd.ExecuteReader();
-        //        while (reader.Read())
-        //        {
-        //            var pokemon = new Pokemon();
-        //            pokemon.PokedexNumber = reader.GetInt32(0);
-        //            pokemon.Name = reader.GetString(1);
-        //            pokemon.Abilities = reader.GetString(2);
-        //            pokemon.Type1 = reader.GetString(3);
-        //            if (reader.IsDBNull(Convert.ToInt32(pokemon.Type2))) pokemon.Type2 = "";
-        //            else pokemon.Type2 = reader.GetString(4);
-        //            pokemon.Hp = reader.GetInt32(5);
-        //            pokemon.Attack = reader.GetInt32(6);
-        //            pokemon.Defense = reader.GetInt32(7);
-        //            pokemon.SpAtk = reader.GetInt32(8);
-        //            pokemon.SpDef = reader.GetInt32(9);
-        //            pokemon.Speed = reader.GetInt32(10);
-        //            pokemon.BaseEggSteps = reader.GetInt32(11);
-        //            pokemon.BaseHappiness = reader.GetInt32(12);
-        //            pokemon.CaptureRate = reader.GetInt32(13);
-        //            pokemon.Classification = reader.GetString(14);
-        //            pokemon.ExperienceGrowth = reader.GetInt32(15);
-        //            if (reader.IsDBNull(Convert.ToInt32(pokemon.HeightM))) pokemon.HeightM = null;
-        //            else pokemon.HeightM = reader.GetFloat(16);
-        //            if (reader.IsDBNull(Convert.ToInt32(pokemon.WeightKg))) pokemon.HeightM = null;
-        //            else pokemon.WeightKg = reader.GetFloat(17);
-        //            if (reader.IsDBNull(Convert.ToInt32(pokemon.PercentageMale))) pokemon.HeightM = null;
-        //            else pokemon.PercentageMale = reader.GetFloat(18);
-        //            pokemon.Generation = reader.GetInt32(19);
-        //            pokemon.Legendary = reader.GetBoolean(20);
-        //            pokemon.AgainstBug = reader.GetFloat(21);
-        //            pokemon.AgainstDark = reader.GetFloat(22);
-        //            pokemon.AgainstDragon = reader.GetFloat(23);
-        //            pokemon.AgainstElectric = reader.GetFloat(24);
-        //            pokemon.AgainstFairy = reader.GetFloat(25);
-        //            pokemon.AgainstFight = reader.GetFloat(26);
-        //            pokemon.AgainstFire = reader.GetFloat(27);
-        //            pokemon.AgainstFlying = reader.GetFloat(28);
-        //            pokemon.AgainstGhost = reader.GetFloat(29);
-        //            pokemon.AgainstGrass = reader.GetFloat(30);
-        //            pokemon.AgainstGround = reader.GetFloat(31);
-        //            pokemon.AgainstIce = reader.GetFloat(32);
-        //            pokemon.AgainstNormal = reader.GetFloat(33);
-        //            pokemon.AgainstPoison = reader.GetFloat(34);
-        //            pokemon.AgainstPsychic = reader.GetFloat(35);
-        //            pokemon.AgainstRock = reader.GetFloat(36);
-        //            pokemon.AgainstSteel = reader.GetFloat(37);
-        //            pokemon.AgainstWater = reader.GetFloat(38);
-
-        //            pokemonCollection.Add(pokemon);
-        //        }
-        //        return pokemonCollection;
-        //    }
-        //    else return null;
-        //}
+        }        
     }
 }
