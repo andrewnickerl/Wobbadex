23c9bffd2822bde4f7172e60317d2a326894acab
Andrew Nickerl <andrew.nickerl@gmail.com>
2020-05-09 15:10:19 -0400

    Created WobbaUser class to model database data for users

---
+++ code/WobbadexClassLibrary/WobbaUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WobbadexClassLibrary
+{
+    public class WobbaUser
+    {
+        public string UserName { get; set; }
+        public string PwHash { get; set; }
+        public int UserId { get; set; }       
+    }
+}
